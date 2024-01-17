d2b9bbb4010bd25a166ed7dae00a3e23854cddcf
Piotr Zarzycki <pan.bobon@gmail.com>
2024-01-17 22:58:52 +0100

    Virus :)

--- lekcja 9 messagebox.py
+++ lekcja 9 messagebox.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
+def hello():
+    while True:
+        messagebox.showinfo("Virus", "You have a VIRUS!!!")
 
 window = Tk()
-window.title("messagebox")
+window.title("siema")
+button = Button(window, command=hello, text="CLICK ME!")
+button.pack()
 window.mainloop()
