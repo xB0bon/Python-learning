f81f447a248733ea7bdc34f90d927e7cf9cf9099
Piotr Zarzycki <pan.bobon@gmail.com>
2024-01-18 13:18:38 +0100

    nowa lekcja

--- lekcja 11 textarea.py
+++ lekcja 11 textarea.py
@@ -1,1 +1,17 @@
 from tkinter import *
+
+
+def sumbit():
+    input = text.get(1.0, END)
+    print(input)
+
+
+window = Tk()
+
+text = Text(window, font=("Ink Free", 25), width=20, height=8, bg="light yellow", fg="black", padx=20, pady=20)
+text.pack()
+
+buttom = Button(window, text="sumbit", command=sumbit)
+buttom.pack()
+
+window.mainloop()
