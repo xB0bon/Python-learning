905aa4622f70ee1f63ae4f8fdd151f2f98174ab1
Piotr Zarzycki <pan.bobon@gmail.com>
2024-01-23 20:12:21 +0100

    teste

--- lekcja 1 windows.py
+++ lekcja 1 windows.py
@@ -1,8 +1,12 @@
 from tkinter import *
+def siema():
+    print("siema")
+
 
 window = Tk()
 window.title("Test")
 window.geometry("400x400")
 window.config(background="black")
+
 
 window.mainloop()
