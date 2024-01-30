7ab245c8e80539391f39411ee34cb95c56925cc4
xB0bon <pan.bobon@gmail.com>
2024-01-30 21:08:17 +0100

    new lesson

---
+++ lekcja 25 animations.py
@@ -0,0 +1,65 @@
+from tkinter import *
+
+import time
+
+
+
+WIDTH = 1500
+
+HEIGHT = 800
+
+xVelocity = 5
+
+yVelocity = 5
+
+window = Tk()
+
+
+
+canvas = Canvas(window,width=WIDTH,height=HEIGHT)
+
+canvas.pack()
+
+
+
+background_photo = PhotoImage(file='z1.png')
+
+background = canvas.create_image(0,0,image=background_photo,anchor=NW)
+
+
+
+photo_image = PhotoImage(file='z2.png')
+
+my_image = canvas.create_image(0,0,image=photo_image,anchor=NW)
+
+
+
+image_width = photo_image.width()
+
+image_height = photo_image.height()
+
+
+
+while True:
+
+    coordinates = canvas.coords(my_image)
+
+    print(coordinates)
+
+    if(coordinates[0]>=(WIDTH-image_width) or coordinates[0]<0):
+
+        xVelocity = -xVelocity
+
+    if(coordinates[1]>=(HEIGHT-image_height) or coordinates[1]<0):
+
+        yVelocity = -yVelocity
+
+    canvas.move(my_image,xVelocity,yVelocity)
+
+    window.update()
+
+    time.sleep(0.01)
+
+
+
+window.mainloop()
